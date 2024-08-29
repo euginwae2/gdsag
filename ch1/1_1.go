df894d607d847f0b1ea79efc3b5d5db0e6a32fea
living-room-desktop <euginwae@gmail.com>
2024-08-29 13:48:24 -0700

    include generics

--- ch1/1_1.go
+++ ch1/1_1.go
@@ -2,41 +2,61 @@ package main
 
 import "fmt"
 
+type Stringer = interface {
+	String() string
+}
+
+type Integer int
+
+func (i Integer) String() string {
+	return fmt.Sprintf("%d", i)
+}
+
+type String string
+
+func (s String) String() string {
+	return string(s)
+}
+
 type Student struct {
 	Name string
 	ID   int
-	age  float64
+	Age  float64
 }
 
-func addStudent(students []string, student string) []string {
+func (s Student) String() string {
+	return fmt.Sprintf("%s %d %0.2f", s.Name, s.ID, s.Age)
+}
+
+func addStudent[T Stringer](students []T, student T) []T {
 	return append(students, student)
 }
 
-func addStudentID(students []int, student int) []int {
-	return append(students, student)
-}
+// func addStudentID(students []int, student int) []int {
+// 	return append(students, student)
+// }
 
-func addStudentStruct(students []Student, student Student) []Student {
-	return append(students, student)
-}
+// func addStudentStruct(students []Student, student Student) []Student {
+// 	return append(students, student)
+// }
 
 func main() {
-	students := []string{} //empty slice
-	result := addStudent(students, "Micheal")
-	result = addStudent(result, "Jennifer")
-	result = addStudent(result, "Elaine")
+	students := []String{} //empty slice
+	result := addStudent[String](students, "Micheal")
+	result = addStudent[String](result, "Jennifer")
+	result = addStudent[String](result, "Elaine")
 	fmt.Println(result)
 
-	students1 := []int{} //empty slice
-	results1 := addStudentID(students1, 155)
-	results1 = addStudentID(results1, 122)
-	results1 = addStudentID(results1, 120)
+	students1 := []Integer{} //empty slice
+	results1 := addStudent[Integer](students1, 155)
+	results1 = addStudent[Integer](results1, 122)
+	results1 = addStudent[Integer](results1, 120)
 
 	fmt.Println(results1)
 
 	students2 := []Student{} //Empty slice
-	results2 := addStudentStruct(students2, Student{"John", 213, 17.5})
-	results2 = addStudentStruct(results2, Student{"James", 111, 18.75})
-	results2 = addStudentStruct(results2, Student{"Marsha", 110, 16.25})
+	results2 := addStudent[Student](students2, Student{"John", 213, 17.5})
+	results2 = addStudent[Student](results2, Student{"James", 111, 18.75})
+	results2 = addStudent[Student](results2, Student{"Marsha", 110, 16.25})
 	fmt.Println(results2)
 }
