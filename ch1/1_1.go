ac5b451616c72d835380d38b36556fcac31c1493
living-room-desktop <euginwae@gmail.com>
2024-08-29 13:26:16 -0700

    add student & student id

--- ch1/1_1.go
+++ ch1/1_1.go
@@ -6,10 +6,23 @@ func addStudent(students []string, student string) []string {
 	return append(students, student)
 }
 
+func addStudentID(students []int, student int) []int{
+	return append(students,student)
+}
+
 func main() {
 	students := []string{} //empty slice
 	result := addStudent(students, "Micheal")
 	result = addStudent(result, "Jennifer")
 	result = addStudent(result, "Elaine")
-	fmt.Print(result)
+	fmt.Println(result)
+
+	students1 := []int{} //empty slice
+	results1 := addStudentID(students1,155)
+	results1 = addStudentID(results1,122)
+	results1 = addStudentID(results1,120)
+
+	fmt.Println(results1)
+	
+	
 }
