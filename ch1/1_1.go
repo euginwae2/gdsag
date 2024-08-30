dc8095f92a47d2d816e51a8d246e79aa7c3c6fd1
living-room-desktop <euginwae@gmail.com>
2024-08-29 17:58:05 -0700

    implement generic map

--- ch1/1_1.go
+++ ch1/1_1.go
@@ -96,6 +96,14 @@ func MyMap(input []int, f func(int) int) []int{
 	return result
 }
 
+func GenericMap[T1,T2 any](input []T1,f func(T1)T2) []T2 {
+	result := make([]T2, len(input))
+	for index, value := range input {
+		result[index] = f(value)
+	}
+	return result
+}
+
 func main() {
 	students := []string{} //empty slice
 	result := addStudent[string](students, "Micheal")
