aeb3caa4146882caa26292ff6133137a3282af98
living-room-desktop <euginwae@gmail.com>
2024-08-29 18:29:15 -0700

    complete generic

--- ch1/1_1.go
+++ ch1/1_1.go
@@ -43,60 +43,59 @@ func addStudent[T any](students []T, student T) []T {
 // 	return append(students, student)
 // }
 
-
 // Group of functions that ensure that an OrderedSlice can be sorted
 
 type Ordered interface {
 	~int | ~float64 | ~string
 }
- type OrderedSlice[T Ordered] []T //T myst implement < and >
+type OrderedSlice[T Ordered] []T //T myst implement < and >
 
- func (s OrderedSlice[T]) Len() int {
+func (s OrderedSlice[T]) Len() int {
 	return len(s)
- }
+}
 
- func (s OrderedSlice[T]) Less(i,j int) bool {
-	return  s[i] < s[j]
- }
+func (s OrderedSlice[T]) Less(i, j int) bool {
+	return s[i] < s[j]
+}
 
- func (s OrderedSlice[T]) Swap(i,j int) {
+func (s OrderedSlice[T]) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
- }
+}
 
 //  Group of functions that ensure that SortType can be sorted
- type SortType[T any] struct{
-	slice []T
-	compare func(T,T) bool
- }
+type SortType[T any] struct {
+	slice   []T
+	compare func(T, T) bool
+}
 
- func (s SortType[T]) Len() int {
+func (s SortType[T]) Len() int {
 	return len(s.slice)
- }
+}
 
- func (s SortType[T]) Less(i,j int) bool {
+func (s SortType[T]) Less(i, j int) bool {
 	return s.compare(s.slice[i], s.slice[j])
- }
+}
 
- func (s SortType[T]) Swap(i,j int) {
+func (s SortType[T]) Swap(i, j int) {
 	s.slice[i], s.slice[j] = s.slice[j], s.slice[i]
- }
+}
 
 //  end group for SortType
 
-func PerformSort[T any](slice []T,compare func(T,T) bool) {
-	sort.Sort(SortType[T]{slice,compare})
+func PerformSort[T any](slice []T, compare func(T, T) bool) {
+	sort.Sort(SortType[T]{slice, compare})
 }
 
 // Map functions
-func MyMap(input []int, f func(int) int) []int{
-	result := make([]int,len(input))
+func MyMap(input []int, f func(int) int) []int {
+	result := make([]int, len(input))
 	for index, value := range input {
 		result[index] = f(value)
 	}
 	return result
 }
 
-func GenericMap[T1,T2 any](input []T1,f func(T1)T2) []T2 {
+func GenericMap[T1, T2 any](input []T1, f func(T1) T2) []T2 {
 	result := make([]T2, len(input))
 	for index, value := range input {
 		result[index] = f(value)
@@ -104,11 +103,21 @@ func GenericMap[T1,T2 any](input []T1,f func(T1)T2) []T2 {
 	return result
 }
 
-func MyFilter(input []float64, f func (float64) bool) []float64 {
+func MyFilter(input []float64, f func(float64) bool) []float64 {
 	var result []float64
-	for _,value := range input {
+	for _, value := range input {
 		if f(value) {
-			result = append(result,value)
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func GenericFilter[T any](input []T, f func(T) bool) []T {
+	var result []T
+	for _, value := range input {
+		if f(value) {
+			result = append(result, value)
 		}
 	}
 	return result
@@ -133,22 +142,46 @@ func main() {
 	results2 := addStudent[Student](students2, Student{"John", 213, 17.5})
 	results2 = addStudent[Student](results2, Student{"James", 111, 18.75})
 	results2 = addStudent[Student](results2, Student{"Marsha", 110, 16.25})
-	PerformSort[Student](results2,func(s1, s2 Student) bool {
-		return s1.Age <s2.Age // Compare twi Student values
+	PerformSort[Student](results2, func(s1, s2 Student) bool {
+		return s1.Age < s2.Age // Compare twi Student values
 	})
 	fmt.Println(results2)
 
 	// map functions
-	slice := []int{1,5,2,7,4}
-	result3 := MyMap(slice,func(i int) int {
+	slice := []int{1, 5, 2, 7, 4}
+	result3 := MyMap(slice, func(i int) int {
 		return i * i
 	})
 	fmt.Println(result3)
 
 	// myfilter
-	input := []float64{17.3, 11.1,9.9,4.3,12.6}
+	input := []float64{17.3, 11.1, 9.9, 4.3, 12.6}
 	res := MyFilter(input, func(f float64) bool {
-		return f < 10.0 
+		return f < 10.0
 	})
 	fmt.Println(res)
+
+	input1 := []float64{-5.0, -2.0, 4.0, 8.0}
+	result4 := GenericMap[float64, float64](input1, func(f float64) float64 {
+		return f * f
+	})
+	fmt.Println(result4)
+
+	greaterThanfive := GenericFilter[int]([]int{4, 5, 6, 2, 20, 1, 7},
+		func(i int) bool {
+			return i > 5
+		})
+	fmt.Println(greaterThanfive)
+
+	oddNumber := GenericFilter[int]([]int{4, 6, 5, 2, 20, 1, 7},
+		func(i int) bool {
+			return i%2 == 1
+		})
+	fmt.Println(oddNumber)
+
+	lenthGreaterThan3 := GenericFilter[string]([]string{"hello", "or", "the", "maybe"}, func(s string) bool {
+		return len(s) > 3
+	})
+
+	fmt.Println(lenthGreaterThan3)
 }
