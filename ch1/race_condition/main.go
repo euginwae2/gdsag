26e7fc5f2ec11d920e6fb6f3255b2f6345a8af5a
living-room-desktop <euginwae@gmail.com>
2024-08-29 21:03:34 -0700

    mutex

--- ch1/race_condition/main.go
+++ ch1/race_condition/main.go
@@ -10,13 +10,16 @@ const (
 )
 
 var countValue int
+var m sync.Mutex
 
 func main() {
 	var wg sync.WaitGroup
 	wg.Add(number)
 	for i := 0; i < number; i++ {
 		go func() {
+			m.Lock()
 			countValue++
+			m.Unlock()
 			wg.Done()
 		}()
 	}
