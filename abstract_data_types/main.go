44846f38518eaef3b10b77bef935e56dd6c0dd93
living-room-desktop <euginwae@gmail.com>
2024-09-11 12:55:30 -0700

    abstract data type with encapsulation

---
+++ abstract_data_types/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type counter struct {
+	count int
+}
+
+type Counter interface {
+	increment()
+	decrement()
+	reset()
+	getCount() int
+}
+
+func (c *counter) increment() {
+	c.count += 1
+}
+
+func (c *counter) decrement() {
+	c.count -= 1
+}
+
+func (c *counter) reset() {
+	c.count = 0
+}
+
+func (c *counter) getCount() int {
+	return c.count
+}
+
+func main() {
+	myCounter := Counter(&counter{})
+	// The only operations that can be performed on myCounter are specified in the Counter interface
+	myCounter.increment()
+	myCounter.increment()
+	myCounter.reset()
+	myCounter.increment()
+	myCounter.increment()
+	myCounter.increment()
+	myCounter.increment()
+	myCounter.decrement()
+	countValue := myCounter.getCount()
+	fmt.Println(countValue)
+}
